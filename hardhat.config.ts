49b8581a88bd4eac2f1018a313fee6d76cad78bb
지의신 <64007655+usgeeus@users.noreply.github.com>
2023-10-01 14:22:06 +0900

    verify on titangoerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,8 @@ import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-typechain'
 import 'hardhat-watcher'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const LOW_OPTIMIZER_COMPILER_SETTINGS = {
   version: '0.7.6',
@@ -51,6 +53,26 @@ export default {
     hardhat: {
       allowUnlimitedContractSize: false,
     },
+    localhost: {
+      forking: {
+        url: `https://rpc.titan-goerli.tokamak.network`,
+      },
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    },
+    titangoerli: {
+      url: 'https://rpc.titan-goerli.tokamak.network',
+      accounts: [`${process.env.PRIVATE_KEY}`],
+      chainId: 5050,
+      // gasPrice: 250000,
+      deploy: ['deploy'],
+    },
+    titan: {
+      url: 'https://rpc.titan.tokamak.network',
+      accounts: [`${process.env.PRIVATE_KEY}`],
+      chainId: 55004,
+      // gasPrice: 250000,
+      // deploy: ['deploy_titan'],
+    },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
     },
@@ -82,7 +104,30 @@ export default {
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    // apiKey: `${process.env.ETHERSCAN_API_KEY}`
+    apiKey: {
+      goerli: `${process.env.ETHERSCAN_API_KEY}`,
+      titangoerli: `${process.env.ETHERSCAN_API_KEY}`,
+      titan: `${process.env.ETHERSCAN_API_KEY}`,
+    },
+    customChains: [
+      {
+        network: 'titangoerli',
+        chainId: 5050,
+        urls: {
+          apiURL: 'https://explorer.titan-goerli.tokamak.network/api',
+          browserURL: 'https://explorer.titan-goerli.tokamak.network',
+        },
+      },
+      {
+        network: 'titan',
+        chainId: 55004,
+        urls: {
+          apiURL: 'https://explorer.titan.tokamak.network/api',
+          browserURL: 'https://explorer.titan.tokamak.network',
+        },
+      },
+    ],
   },
   solidity: {
     compilers: [DEFAULT_COMPILER_SETTINGS],
---
+++ scripts/verify.ts
@@ -0,0 +1,70 @@
+import hre from 'hardhat'
+import fs from 'fs'
+const run = hre.run
+const chainName = hre.network.name
+
+let data = JSON.parse(fs.readFileSync(`deployed.uniswap.${chainName}.json`).toString())
+
+const main = async () => {
+  console.log('Verifying contract...')
+  try {
+    await run('verify', {
+      address: data['NonfungiblePositionManager'],
+      constructorArgsParams: [
+        data['UniswapV3Factory'],
+        '0x4200000000000000000000000000000000000006',
+        data['NonfungibleTokenPositionDescriptor'],
+      ],
+    })
+    await run('verify', {
+      address: data['SwapRouter'],
+      constructorArgsParams: [data['UniswapV3Factory'], '0x4200000000000000000000000000000000000006'],
+    })
+    await run('verify', {
+      address: data['NFTDescriptor'],
+      constructorArgsParams: [],
+    })
+    await run('verify', {
+      address: data['NonfungibleTokenPositionDescriptor'],
+      constructorArgsParams: [
+        '0x4200000000000000000000000000000000000006',
+        '0xaaaebeba3810b1e6b70781f14b2d72c1cb89c0b2b320c43bb67ff79f562f5ff4',
+      ],
+    })
+    await run('verify', {
+      address: data['NonfungiblePositionManager'],
+      constructorArgsParams: [
+        data['UniswapV3Factory'],
+        '0x4200000000000000000000000000000000000006',
+        data['NonfungibleTokenPositionDescriptor'],
+      ],
+    })
+    await run('verify', {
+      address: data['Quoter'],
+      constructorArgsParams: [data['UniswapV3Factory'], '0x4200000000000000000000000000000000000006'],
+    })
+    await run('verify', {
+      address: data['QuoterV2'],
+      constructorArgsParams: [data['UniswapV3Factory'], '0x4200000000000000000000000000000000000006'],
+    })
+    await run('verify', {
+      address: data['TickLens'],
+      constructorArgsParams: [],
+    })
+    await run('verify', {
+      address: data['UniswapInterfaceMulticall'],
+      constructorArgsParams: [],
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes('already verified')) {
+      console.log('Already verified!')
+    } else {
+      console.log(e)
+    }
+  }
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
