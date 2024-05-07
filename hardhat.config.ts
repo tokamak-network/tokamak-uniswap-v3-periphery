21030b41922fc59028f5b42ce757fe8667af24cf
지의신 <64007655+usgeeus@users.noreply.github.com>
2024-05-07 14:33:39 +0900

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -92,7 +92,10 @@ export default {
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: 1,
+    apiKey: {
+      thanossepolia: 1,
+      titansepolia: 1,
+    },
     customChains: [
       {
         network: 'thanossepolia',
